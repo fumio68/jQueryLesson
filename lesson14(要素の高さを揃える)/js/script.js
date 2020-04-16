eba3586a3f25e7d860d356b75fc30c0bb595b167
fumio68 <rgkk.5123@gmail.com>
2020-04-16 15:54:05 +0900

    lesson14(要素の高さを揃える)

---
+++ lesson14(要素の高さを揃える)/js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+  let h = 0;
+
+  $("main div").each(function(){
+    if($(this).height() > h){
+      h = $(this).height();
+    }
+  });
+
+  $("main div").css("height", h + "px");
+});
