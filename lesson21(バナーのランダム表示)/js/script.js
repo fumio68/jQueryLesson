0e9e5947eef898054738bf804ab9b3221e461e69
fumio68 <rgkk.5123@gmail.com>
2020-04-16 19:23:21 +0900

    lesson21(バナーのランダム表示)

---
+++ lesson21(バナーのランダム表示)/js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+  let bannerArray = ["red", "yellow", "green", "blue", "brown"];
+
+  let num = Math.random();
+
+  num = num * 5;
+
+  num = Math.floor(num);
+
+  $("aside img").attr("src", "img/" + bannerArray[num] + ".jpg")
+});
