86b893523c68ef27ef2811d234c61cc63cf41f9f
fumio68 <rgkk.5123@gmail.com>
2020-04-16 18:36:58 +0900

    lesson20(スムーススクロール)

---
+++ lesson20(スムーススクロール)/js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+  $("a[href^=#]:not([href=#])").click(function(){
+
+    let target = $($(this).attr("href")).offset().top;
+
+    target -= 70;
+
+    $("html, body").animate({scrollTop: target}, 500);
+
+    return false;
+  });
+});
