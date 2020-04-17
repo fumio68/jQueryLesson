00f5ac5d94dc15e956dd03d118c3f62c2818ced6
fumio68 <rgkk.5123@gmail.com>
2020-04-17 23:35:38 +0900

    lesson25(ブラウザ上部に固定されるヘッダー)

---
+++ lesson25(ブラウザ上部に固定されるヘッダー)/js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+  let navPos = $("nav").offset().top;
+
+  $(window).scroll(function(){
+    if($(window).scrollTop() > navPos){
+      $("nav").css("position", "fixed");
+    }else{
+      $("nav").css("position", "static");
+    }
+  });
+});
